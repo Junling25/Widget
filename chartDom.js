aeaffc0c7103b507ecd0ddb5a94d774759717e36
Junling25 <132639751+Junling25@users.noreply.github.com>
2023-09-15 15:11:19 +0800

    Update chartDom.js

--- chartDom.js
+++ chartDom.js
@@ -5,19 +5,19 @@ var getScriptPromisify = (src) => {
 }
 
 (function () {
-  const chartdom = document.createElement('template')
-  chartdom.innerHTML = `
+  const bardemotest = document.createElement('template')
+  bardemotest.innerHTML = `
       <style>
       </style>
       <div id="root" style="width: 100%; height: 100%;">
       </div>
     `
-  class Sampledemo extends HTMLElement {
+  class Samplebardemo extends HTMLElement {
     constructor () {
       super()
 
       this._shadowRoot = this.attachShadow({ mode: 'open' })
-      this._shadowRoot.appendChild(chartdom.content.cloneNode(true))
+      this._shadowRoot.appendChild(bardemotest.content.cloneNode(true))
 
       this._root = this._shadowRoot.getElementById('root')
 
@@ -26,112 +26,109 @@ var getScriptPromisify = (src) => {
       this.render()
     }
 
-    async render (resultSet2) {
+    async render (resultSet) {
       await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js')
 
       const chart = echarts.init(this._root)
+      
+      var month = [];
 
-      const region = []
-      const channel = []
-
-      var revenueA =[];
-      var revenueB =[];
-      var revenueC =[];
-      var revenueD =[];
-
-      for (var i=0; i<resultSet2.length;i++){
+      var sale =[];
+      var revenue =[];
+ 
+       for (var i=0; i<resultSet.length;i++){
 	
-        	var pro = resultSet2[i]["ID_4j181g05s5"].description;
-	
-          if (region.indexOf(pro) === -1) {
-           region.push(pro);
-           }
-	
-	
-	        var chan = resultSet2[i]["ID_2r2z313k3n"].description;
-        	 if (channel.indexOf(chan) === -1) {
-           channel.push(chan);
-           }
+	     if(resultSet[i]["@MeasureDimension"].description==="销量")
+	     {
+        	var sales = Number(resultSet[i]["@MeasureDimension"].rawValue);
+        	sale.push(sales);
+      	}
+    	if(resultSet[i]["@MeasureDimension"].description ==="收入"){
+	       var revenues = Number(resultSet[i]["@MeasureDimension"].rawValue);
+      	 revenue.push(revenues);
+	     }
 	
 
-	       if (resultSet2[i]["ID_2r2z313k3n"].description ==="电商") {
-	      	var revenue = Number(resultSet2[i]["@MeasureDimension"].rawValue);
-	      	revenueA.push(revenue);
-        	}
-
-         	else if (resultSet2[i]["ID_2r2z313k3n"].description ==="百货") {
-	      	revenue = Number(resultSet2[i]["@MeasureDimension"].rawValue);
-      		revenueB.push(revenue);
-        	}
+	     var month1=resultSet[i]["ID_4h4o2c3k3n"].description;
+	      if (month.indexOf(month1) === -1) {
+          month.push(month1);
+        }
 	
-        	else if (resultSet2[i]["ID_2r2z313k3n"].description ==="直营") {
-	        revenue = Number(resultSet2[i]["@MeasureDimension"].rawValue);
-        	revenueC.push(revenue);
-        	}
-	        else if (resultSet2[i]["ID_2r2z313k3n"].description ==="购物") {
-	        revenue = Number(resultSet2[i]["@MeasureDimension"].rawValue);
-       	revenueD.push(revenue);
-       	}
 }
-
-      console.log(region)
-      console.log(channel)
+console.log(sale);
+console.log(revenue);
+console.log(month);
 
       const option = {
-        angleAxis: {
-          type: 'category',
-          data: region
+        title: {
+          text: '',
       },
-      radiusAxis: {
+      tooltip: {
+          trigger: 'axis'
       },
-      polar: {
-      },
-      series: [{
-          type: 'bar',
-          data: revenueA,
-          coordinateSystem: 'polar',
-          name: '电商',
-          stack: 'a',
-          emphasis: {
-              focus: 'series'
-          }
-      }, {
-          type: 'bar',
-          data: revenueB,
-          coordinateSystem: 'polar',
-          name: '百货',
-          stack: 'a',
-          emphasis: {
-              focus: 'series'
-          }
-      }, {
-          type: 'bar',
-          data: revenueC,
-          coordinateSystem: 'polar',
-          name: '直营',
-          stack: 'a',
-          emphasis: {
-              focus: 'series'
-          }
-      },{
-        type: 'bar',
-        data: revenueD,
-        coordinateSystem: 'polar',
-        name: '购物',
-        stack: 'a',
-        emphasis: {
-            focus: 'series'
-        }
-    },],
       legend: {
+          data: ['收入', '销量']
+      },
+      toolbox: {
           show: true,
-          data: channel
-      }
-  };
+          feature: {
+              dataView: {show: true, readOnly: false},
+              magicType: {show: true, type: ['line', 'bar']},
+              restore: {show: true},
+              saveAsImage: {show: true}
+          }
+      },
+      calculable: true,
+      xAxis: [
+          {
+              type: 'category',
+              data: month
+          }
+      ],
+      yAxis: [
+          {
+              type: 'value'
+          }
+      ],
+      series: [
+          {
+              name: '收入',
+              type: 'bar',
+              data: revenue,
+              markPoint: {
+                  data: [
+                      {type: 'max', name: '最大值'},
+                      {type: 'min', name: '最小值'}
+                  ]
+              },
+              markLine: {
+                  data: [
+                      {type: 'average', name: '平均值'}
+                  ]
+              }
+          },
+          {
+              name: '销量',
+              type: 'bar',
+              data: sale,
+              markPoint: {
+                  data: [
+                    {type: 'max', name: '最大值'},
+                    {type: 'min', name: '最小值'}
+                  ]
+              },
+              markLine: {
+                  data: [
+                      {type: 'average', name: '平均值'}
+                  ]
+              }
+          }
+      ]
+    };
     
       chart.setOption(option)
     }
   }
 
-  customElements.define('sapecharts-chartdom', Sampledemo)
+  customElements.define('sapecharts-bardemotest', Samplebardemo)
 })()
